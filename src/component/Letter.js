5170af9def21d211af911b97559b4c17513eda52
Gautier Arcin <gauta82@gmail.com>
2020-12-31 20:18:02 +0100

    change speed

--- src/component/Letter.js
+++ src/component/Letter.js
@@ -61,7 +61,7 @@ const Letter = () => {
           options={{
             strings: retrievedData,
             cursorClassName: className,
-            delay: 16,
+            delay: 17,
             // delay: 2,
             autoStart: true,
             loop: false,
