1c38190a6024b657e0eb1f4a3da0cffd27f20426
Gautier Arcin <gauta82@gmail.com>
2021-03-21 23:08:00 +0100

    galadrim

--- src/component/Letter.js
+++ src/component/Letter.js
@@ -1,83 +1,182 @@
 import "./Letter.css";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useMediaQuery } from "react-responsive";
 import TypeIt from "typeit-react";
 
-import Timer from "./Timer";
-
-const getRandomArbitrary = (min, max) => {
-  const rand = Math.random() * (max - min) + min;
-  // console.log("random : " + rand);
-  return rand;
-};
-const NewlineText = (text) => {
-  const arr = text.split("\n").map((str) =>
-    str
-      .replace(/([.?!])\s*(?=[A-Z])/g, "$1|")
-      .split("|")
-      .map((e) => `<span class=letter>${e} </span>`)
-  );
-
-  const toFilter = ["<span class=letter> </span>"];
-  const newText = arr.filter((liste) => liste[0] !== toFilter[0]);
-  return newText;
-};
-
-const formatLetter = (props) => {
-  let stringToReturn = [
-    ...props["text"],
-    [
-      `<p class="letter endline">  <a href=${props["url"]}> ${props["title"]} </a>, written by <span class=author> ${props["author"]} </span> </p>  `,
-      // `<div class="letter endline" style="font-size:0.5em;">Icons made by <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a></div> <br>`,
-      //`<div class="letter endline" style="font-size:0.8em;">Made with 💖 by <a href="https://www.linkedin.com/in/gautier-arcin-309405145/" title="Gautier Arcin">Gautier Arcin</a>. Icons made by <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a>.</div>`,
-    ],
-  ];
-  return stringToReturn;
-};
-
-const letterToSequence = ({
-  formatedLetter,
-  setterFunctionInit,
-  marginBottom,
-}) => {
+const letterToSequence = ({ setterFunctionInit, marginBottom }) => {
   const tempo = () => (instance) => {
-    const len = formatedLetter.length;
-    // eslint-disable-next-line
-    formatedLetter.map((bigArray, i) => {
-      instance.type('<br style="margin-bottom: ' + marginBottom + 'em;">');
-      if (i + 1 !== len) {
-        bigArray.map(
-          (e) => (
-            // eslint-disable-next-line
-            instance.type(e), instance.pause(getRandomArbitrary(400, 900))
-          )
-        );
-        instance.pause(getRandomArbitrary(600, 1400));
-      } else {
-        // cursor.style.visibility = "hidden";
-        instance.exec(async () => {
-          document.getElementsByClassName("ti-cursor")[0].style.visibility =
-            "hidden";
-        });
-        instance.type(formatedLetter[i][0]);
-        // instance.type(formatedLetter[i][1]);
-        instance.break();
-        instance.break();
-        instance.break();
-      }
-    });
+    instance.type("<span class=letter>Salut Galadrim 👋 !</span>");
+    instance.pause(2500);
+    instance.type('<br style="margin-bottom: ' + marginBottom + 'em;">');
 
-    // instance.move(-5, { speed: 1 });
+    instance.type("<span class=letter>Je m'appelle gautier arcin.</span>");
+    instance.pause(1000);
+    instance.move(-5, { speed: 45 });
+    instance.pause(300);
+    instance.delete(1);
+    instance.pause(300);
+    instance.type("<span class=letter>A</span>");
+    instance.pause(300);
+    instance.move(-8, { speed: 45 });
+    instance.pause(300);
+    instance.delete(1);
+    instance.pause(300);
+    instance.type("<span class=letter>G</span>");
+    instance.pause(700);
+    instance.move("END", { speed: 40 });
+    instance.pause(1000);
+
+    instance.type("<span class=letter> Je suis ingénieur full-st<span>");
+    instance.pause(400);
+    instance.delete(7, { speed: 200 });
+    instance.pause(300);
+    instance.type("<span class=letter> robotique,</span>");
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> désireux de se reconvertir en ingénieur full-stack <span>"
+    );
+    instance.pause(300);
+    instance.type("<span class=letter>(full-remote).<span>", { speed: 60 });
+    instance.pause(2000);
+    instance.type('<br style="margin-bottom: ' + marginBottom + 'em;">');
+    instance.pause(500);
+
+    instance.type(
+      "<span class=letter>Je suis diplômé de l'école d'ingénieur Upssitech, </span>"
+    );
+    instance.pause(300);
+    instance.type(
+      '<span class=letter style="font-size: 1.3em !important; "> (une glorieuse rang C), </span>'
+    );
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> en <b>S</b>ystèmes <b>R</b>obotiques et <b>I</b>nteractifs. </span>"
+    );
+    instance.pause(1500);
+
+    instance.type(
+      "<span class=letter>Concernant mon parcours professionnel,</span>"
+    );
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> j'ai eu l'opportunité d'effectuer un stage en Espagne, </span>"
+    );
+    instance.pause(300);
+    instance.type("<span class=letter> en M1,</span>");
+    instance.pause(200);
+    instance.type(
+      '<span class=letter> qui a abouti sur la publication d\'un <a href="https://link.springer.com/article/10.1007/s00170-020-06234-5" >article scientifique</a>. </span>'
+    );
+    instance.pause(1500);
+
+    instance.type(
+      "<span class=letter>J'ai ensuite pû travailler en tant qu'ingénieur robotique spatiale </span>"
+    );
+    instance.pause(300);
+    instance.type(
+      '<span class=letter style="font-size: 1.3em !important; ">(en alternance)</span>'
+    );
+    instance.pause(300);
+    instance.type(
+      '<span class=letter> sur le projet <a href="https://www.h2020-pulsar.eu/" > H2020 Pulsar</a>,</span>'
+    );
+    instance.pause(300);
+    instance.type(
+      '<span class=letter> issu du plan <a href="https://www.esa.int/Enabling_Support/Space_Engineering_Technology/PERASPERA_Space_Robotic_Technologies" >PERESPERA</a> de l\'<b>E</b>uropean <b>S</b>pace <b>A</b>gency.</span>'
+    );
+    instance.pause(1500);
+
+    instance.type("<span class=letter> Finalement,</span>");
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> j'occupe actuellement un poste d'ingénieur d'études à l'IRIT,</span>"
+    );
+    instance.pause(300);
+    instance.type(
+      '<span class=letter> sur le <a href="https://linto.ai/"> projet Linto</a>, </span>'
+    );
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> où je travaille sur des thématiques de data scientist.</span>"
+    );
+    instance.pause(2000);
+    instance.type('<br style="margin-bottom: ' + marginBottom + 'em;">');
+    instance.pause(500);
+
+    instance.type(
+      "<span class=letter>J'ai découvert ✨ Galadrim ✨ au contact de Mathieu,</span>"
+    );
+    instance.type("<span class=letter> Valentin et Pierre. </span>");
+    instance.pause(1500);
+    instance.type("<span class=letter>Grâce à eux,</span>");
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> j'ai pu découvrir le métier d'ingénieur full-stack, </span>"
+    );
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> dans lequel j'ai envie de me reconvertir, </span>"
+    );
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> de par la polyvalence et l'excellence technique nécessaires pour l'exercer. </span>"
+    );
+    instance.pause(1500);
+
+    instance.type(
+      "<span class=letter>J'ai aujourd'hui des compétences en front-end (react) via des sites que j'ai pû réaliser </span>"
+    );
+    instance.pause(300);
+    instance.type(
+      '<span class=letter>(<a href="https://demo-linto.netlify.app/">Linto-demo</a>,</span>'
+    );
+    instance.pause(300);
+    instance.type("<span class=letter> par exemple, </span>");
+    instance.pause(300);
+    instance.type(
+      "<span class=letter> un site pour présenter des travaux de recherches), </span>"
+    );
+    instance.pause(300);
+    instance.type(
+      "<span class=letter>mais il me reste un nombre de compétences manquantes en backend. </span>"
+    );
+    instance.pause(1500);
+    instance.type(
+      "<span class=letter>Je travaille actuellement à les acquerir en multipliants les projets personnels.</span>"
+    );
+    instance.pause(2000);
+    instance.type('<br style="margin-bottom: ' + marginBottom + 'em;">');
+    instance.pause(500);
+
+    instance.type(
+      '<span class=letter>Vous pouvez me joindre sur mon <a href="https://www.linkedin.com/in/gautier-arcin-309405145/"> Linkedin</a>, </span>'
+    );
+    instance.pause(300);
+    instance.type(
+      '<span class=letter>mon mail <a href="mailto:gautierarcin@gmail.com"> gautierarcin@gmail.com</a>, </span>'
+    );
+    instance.pause(300);
+    instance.type(
+      '<span class=letter>ou au <span style="color:blue"> 0606792898. </span> </span>'
+    );
+
+    instance.pause(2000);
+    instance.type('<br style="margin-bottom: ' + marginBottom + 'em;">');
+    instance.pause(500);
+
+    instance.type(
+      '<span class=letter style="font-size: 2.4em !important;">Merci pour votre attention ! 😁<span>'
+    );
+    instance.pause(1500);
 
     return instance;
   };
-  console.log("séquence : " + tempo);
+  // console.log("séquence : " + tempo);
 
+  console.log("Salut :)");
   setterFunctionInit(tempo);
 };
-
-const increaseValue = (value, Setter) => Setter(value + 1);
 
 const Letter = ({ cpt, setterCpt }) => {
   const [functionTypewriter, setFunctionTypewriter] = useState(
@@ -87,11 +186,8 @@ const Letter = ({ cpt, setterCpt }) => {
     }
   );
   const [delay, setDelay] = useState(26);
-  const [isFinished, setIsFinished] = useState(0);
   const [instance, setInstance] = useState(null);
   const [paused, setPaused] = useState(false);
-
-  const refTypeIt = useRef(null);
 
   //Tablet or mobile
   const isTabletOrMobile = useMediaQuery({
@@ -99,72 +195,17 @@ const Letter = ({ cpt, setterCpt }) => {
   });
 
   useEffect(() => {
-    fetch(
-      "https://www.reddit.com/r/unsentletters/random.json?&raw_json=1"
-    ).then(
-      (url) => (
-        // eslint-disable-next-line
-        console.log(url),
-        fetch(url["url"]).then((res) =>
-          res.json().then((response) => {
-            try {
-              if (response[0].data.children[0].data.selftext) {
-                const text = NewlineText(
-                  response[0].data.children[0].data.selftext
-                );
-                const tempDic = {
-                  text: text,
-                  author: "u/" + response[0].data.children[0].data.author,
-                  title: response[0].data.children[0].data.title,
-                  url: response[0].data.children[0].data.url,
-                };
-                const totalString = formatLetter(tempDic);
-                letterToSequence({
-                  formatedLetter: totalString,
-                  setterFunctionInit: setFunctionTypewriter,
-                  marginBottom: isTabletOrMobile ? "3.4" : "2.5",
-                });
-              }
-            } catch (e) {
-              console.log(e);
-            }
-          })
-        )
-      )
-    );
-
     setDelay(isTabletOrMobile ? 46 : 36);
+    letterToSequence({
+      setterFunctionInit: setFunctionTypewriter,
+      marginBottom: isTabletOrMobile ? "3.4" : "2.5",
+    });
     // eslint-disable-next-line
   }, []);
 
-  useEffect(() => {
-    console.log("value : " + isFinished);
-  }, [isFinished]);
-
-  useEffect(() => {
-    console.log("instance :");
-    console.log(instance);
-    console.log(document.getElementsByClassName("ti-cursor")[0]);
-  }, [instance]);
-
-  useEffect(() => {
-    console.log("ref :");
-    console.log(refTypeIt.current.children[0]);
-  }, [refTypeIt]);
-
-  const textInput = useRef(null);
-  useEffect(() => {
-    console.log("ref 2 :");
-    console.log(textInput);
-  }, [textInput]);
-
   const [allowChange, setAllowChange] = useState(true);
 
-  // const allowChangeRef = useRef(allowChange);
-  // allowChangeRef.current = allowChange;
-
   useEffect(() => {
-    console.log(paused);
     if (instance != null) {
       if (paused) {
         if (!instance.is("frozen")) {
@@ -175,7 +216,7 @@ const Letter = ({ cpt, setterCpt }) => {
           setTimeout(() => {
             instance.unfreeze();
             setAllowChange(true);
-          }, 400);
+          }, 300);
         }
       }
     }
@@ -208,23 +249,19 @@ const Letter = ({ cpt, setterCpt }) => {
       }}
       className="wrapper"
     >
-      <div className="center" ref={refTypeIt}>
+      <div className="center">
         <TypeIt
           getBeforeInit={(e) => functionTypewriter(e)}
-          ref={textInput}
           key={functionTypewriter}
           options={{
             speed: delay,
             waitUntilVisible: true,
             lifeLike: true,
-            cursorChar: '<span class="cursor">|</span>',
+            cursorChar: '<span class="cursor letter">|</span>',
 
-            beforeStep: async (step, instance) => {
-              setIsFinished(false);
-            },
+            beforeStep: async (step, instance) => {},
             afterComplete: async (step, instance) => {
               instance.destroy();
-              setIsFinished(true);
             },
           }}
           getAfterInit={(instance) => {
@@ -232,14 +269,6 @@ const Letter = ({ cpt, setterCpt }) => {
             return instance;
           }}
         />
-
-        {isFinished && (
-          // Re-init is finished
-          <Timer
-            paused={paused}
-            function={() => increaseValue(cpt, setterCpt)}
-          ></Timer>
-        )}
       </div>
     </div>
   );
